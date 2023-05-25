9697fa60bfa3a04a193b57ed9395e5a00a7872fc
Maciej Bartoszewski <bartoszewski.maciek@gmail.com>
2023-05-26 00:41:12 +0200

    hw10 test2

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,3 +1,23 @@
+// cat-api.js
+
+export function fetchBreeds() {
+    return fetch('https://api.thecatapi.com/v1/breeds')
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Failed to fetch cat breeds.');
+        }
+        return response.json();
+      })
+      .then(data => {
+        return data.map(breed => {
+          return {
+            id: breed.id,
+            name: breed.name
+          };
+        });
+      });
+  }
+  
 export function fetchCatByBreed(breedId) {
     //const url = `https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`;
     //'api_key=live_41i6SveXjchRpTWZglqfBlRTGUsxB2IfpZfHTW6DSkYdNVCFcelFWp8H2ftmv7Ie'
